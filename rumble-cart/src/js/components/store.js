30150c9e786f61bfab95387977173acd649710b1
Michael Teixeira <meixeira@gmail.com>
2025-05-06 12:20:59 -0400

    have header as standalone component

--- rumble-cart/src/js/components/store.js
+++ rumble-cart/src/js/components/store.js
@@ -1,14 +1,9 @@
+import { renderHeader } from "./header";
+
 export function renderStore() {
     const app = document.getElementById('app');
 
-    const header = document.createElement('header');
-    header.className = 'flex justify-between items-center py-6 mb-8 border-b border-gray-200';
-    header.innerHTML = `
-    <div>
-      <h1 class="text-4xl font-semibold mb-2">Shopping Cart</h1>
-      <p class="text-gray-600">Select your favorite products</p>
-    </div>
-  `;
+    const header = renderHeader();
 
     app.appendChild(header);
 }
