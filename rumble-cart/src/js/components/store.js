38d0cc8cf65495a8837572e6b52e443d0db0e0e8
Michael Teixeira <meixeira@gmail.com>
2025-05-06 12:22:51 -0400

    add placeholder container for product inventory and cart

--- rumble-cart/src/js/components/store.js
+++ rumble-cart/src/js/components/store.js
@@ -4,6 +4,11 @@ export function renderStore() {
     const app = document.getElementById('app');
 
     const header = renderHeader();
+    const container = document.createElement('div');
+    container.className = 'grid grid-cols-1 lg:grid-cols-[1fr_350px] gap-8';
+
+    
 
     app.appendChild(header);
+    app.appendChild(container);
 }
